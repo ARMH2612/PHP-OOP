e82308f955c6da8375931468406e2413c5fc570a
Houssame Rahiche <houssamerahiche@gmail.com>
2022-01-29 21:06:54 +0100

    intro to classes

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    class User{
+
+    }
+
+    $user1 = new User();
+
+    echo get_class($user1);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP OOP Tuts</title>
+</head>
+<body>
+    
+</body>
+</html>
