8409a9ee877b8c4699183b467aa1cdf2445c2b2c
Houssame Rahiche <houssamerahiche@gmail.com>
2022-01-29 21:30:44 +0100

    properties and methodes

--- index.php
+++ index.php
@@ -1,11 +1,30 @@
 <?php
-    class User{
+    class User {
+        // properties
+        public $username = 'houssame';
+        public $email= 'houssame@gmail.com';
 
+        // methodes:
+        public function addFriend(){
+            return $this->username." added a new friend";
+        }
     }
 
     $user1 = new User();
+    $user2 = new User();
 
-    echo get_class($user1);
+    echo $user1->email."<br />";
+    echo $user1->addFriend()."<br />";
+    
+    // to get class' properties:
+    print_r(get_class_vars('User'));
+    echo '<br />';
+    print_r(get_class_methods('User'));
+
+    // changing class' properties values:
+    // methode 1:
+    $user1->username = "<br />Rahiche Houssame";
+    echo $user1->username;
 ?>
 
 <!DOCTYPE html>
